2d90396522ee407f75b73abfd311eec69f6c80af
jaikishanpatil <patiljaikishan4@gmail.com>
2023-03-17 12:56:05 +0530

    navbar added to header component

--- src/app/modules/main-module/sub-pages/header/header.component.ts
+++ src/app/modules/main-module/sub-pages/header/header.component.ts
@@ -13,8 +13,21 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.path = this.router.url;
+    window.addEventListener('scroll',this.isScrolling)
+  }
+  ngOnChanges(): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    const headerel=document.querySelector('.primary-header')
+    let windowpos=window.scrollY>250;
+    headerel?.classList.toggle('active',windowpos)
   }
 
+  isScrolling=()=>{
+    const headerel=document.querySelector('.primary-header')
+    let windowpos=window.scrollY>250;
+    headerel?.classList.toggle('active',windowpos)
+  }
   clickMe() {
     this.path = this.router.url;
     this.router.navigate([`test`]);
