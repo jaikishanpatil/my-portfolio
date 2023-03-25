1c328db10f8528ffd0dd4a67200c6139246eb6fa
jaikishanpatil <patiljaikishan4@gmail.com>
2023-03-25 13:28:19 +0530

    Service tab added

--- src/app/modules/main-module/sub-pages/main/main.component.ts
+++ src/app/modules/main-module/sub-pages/main/main.component.ts
@@ -7,39 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   constructor() {}
-  activeAccordion = 'One'
-  todaysDate:any=new Date().toLocaleDateString()
+  activeAccordion = 'One';
+  todaysDate: any = new Date().toLocaleDateString();
   ngOnInit(): void {
     this.qualificationTabs();
+    this.serviceTab();
   }
 
   //Qualifications tabs
-  qualificationTabs(){
+  qualificationTabs() {
     const tabs = document.querySelectorAll('[data-target]');
-    const tabContents = document.querySelectorAll('[data-content]')
+    const tabContents = document.querySelectorAll('[data-content]');
 
-    tabs.forEach((tab: any)=>{
-      tab.addEventListener('click',()=>{
-        const target = document.querySelector(tab.dataset.target)
-        tabContents.forEach((tabcontent:any)=>{
-          tabcontent.classList.remove('qualification_active')
-        })
-        target.classList.add('qualification_active')
-        tab.forEach((tab:any)=>{
-          tab.classList.remove('qualification_active')
-        })
-        tab.classList.add('qualification_active')
-      })
-    })
+    tabs.forEach((tab: any) => {
+      tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach((tabcontent: any) => {
+          tabcontent.classList.remove('qualification_active');
+        });
+        target.classList.add('qualification_active');
+        tab.forEach((tab: any) => {
+          tab.classList.remove('qualification_active');
+        });
+        tab.classList.add('qualification_active');
+      });
+    });
   }
 
   // skills tab
- 
-  isActive (name: string){
-    return this.activeAccordion === name
+
+  isActive(name: string) {
+    return this.activeAccordion === name;
   }
 
-  setActive(name: string){
-    this.activeAccordion = name
-  } 
+  setActive(name: string) {
+    this.activeAccordion = name;
+  }
+
+  // services tab
+  serviceTab() {
+    const modalView = document.querySelectorAll('.services_model');
+    const modalBtns = document.querySelectorAll('.services_button');
+    const modalClose = document.querySelectorAll('.services_model-close');
+
+    let model = function (modelClick: any) {
+      modalView[modelClick].classList.add('active-modal');
+    };
+    modalBtns.forEach((modalbtn: any, index) => {
+      modalbtn.addEventListener('click', () => {
+        model(index);
+      });
+    });
+    modalClose.forEach((modelClose: any) => {
+      modelClose.addEventListener('click', () => {
+        modalView.forEach((modelvies: any) => {
+          modelvies.classList.remove('active-modal');
+        });
+      });
+    });
+  }
 }
