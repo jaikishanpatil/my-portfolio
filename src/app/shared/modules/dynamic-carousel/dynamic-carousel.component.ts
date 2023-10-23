c49ad62a39b5482ccba4f20ef64c5a969619f2b2
adiflextechnologies <adiflextechnologies@gmail.com>
2023-10-24 00:23:13 +0530

    carousel dots and image transition added

--- src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
+++ src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
@@ -1,21 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-interface CarouselImges{
-  imageSrc:string,
-  imageAlt:string
+interface CarouselImges {
+  imageSrc: string;
+  imageAlt: string;
 }
 @Component({
   selector: 'dynamic-carousel',
   templateUrl: './dynamic-carousel.component.html',
-  styleUrls: ['./dynamic-carousel.component.scss']
+  styleUrls: ['./dynamic-carousel.component.scss'],
 })
 export class DynamicCarouselComponent implements OnInit {
+  @Input() images: CarouselImges[] = [];
+  @Input() indicators: boolean = true;
+  selectedIndex: number = 0;
+  constructor() {}
 
-  @Input() images:CarouselImges[]=[];
-  selectedIndex:number=0;
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  selectImage(index: any) {
+    this.selectedIndex = index;
   }
-
 }
