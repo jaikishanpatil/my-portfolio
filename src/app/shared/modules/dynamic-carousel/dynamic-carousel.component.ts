8e62b97756eae67315f05b58a16d31a9704bf6ef
adiflextechnologies <adiflextechnologies@gmail.com>
2023-10-25 20:55:12 +0530

    Features added: auto slide

--- src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
+++ src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
@@ -13,10 +13,22 @@ export class DynamicCarouselComponent implements OnInit {
   @Input() images: CarouselImges[] = [];
   @Input() indicators: boolean = true;
   @Input() controls: boolean = true;
+  @Input() autoSlide: boolean = false;
+  @Input() slideInterval: number = 3000; //defaault 3 sec
   selectedIndex: number = 0;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.autoSlide) {
+      this.autoSlideImage();
+    }
+  }
+
+  autoSlideImage() {
+    setInterval(() => {
+      this.onNextClick();
+    }, this.slideInterval);
+  }
   selectImage(index: any) {
     this.selectedIndex = index;
   }
