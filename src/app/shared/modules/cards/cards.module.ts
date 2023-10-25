3e9e9d77384083066455f945657509f6961952fc
adiflextechnologies <adiflextechnologies@gmail.com>
2023-10-25 21:43:55 +0530

    cards: profile card design

---
+++ src/app/shared/modules/cards/cards.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfileCardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    ProfileCardComponent
+  ]
+})
+export class CardsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { DynamicCarouselModule } from './modules/dynamic-carousel/dynamic-carousel.module';
+import { CardsModule } from './modules/cards/cards.module';
 
+const modules=[
+  CardsModule,
+  DynamicCarouselModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     SharedRoutingModule,
-    DynamicCarouselModule
+    ...modules
   ],
   exports:[
-    DynamicCarouselModule
+    ...modules
   ]
 })
 export class SharedModule { }
