d4f6cddb954efddd12ad915905d5383faa9f98f9
adiflextechnologies <adiflextechnologies@gmail.com>
2023-10-25 11:56:28 +0530

    buttons added for navigation through images

--- src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
+++ src/app/shared/modules/dynamic-carousel/dynamic-carousel.component.ts
@@ -12,11 +12,26 @@ interface CarouselImges {
 export class DynamicCarouselComponent implements OnInit {
   @Input() images: CarouselImges[] = [];
   @Input() indicators: boolean = true;
+  @Input() controls: boolean = true;
   selectedIndex: number = 0;
   constructor() {}
 
   ngOnInit(): void {}
   selectImage(index: any) {
     this.selectedIndex = index;
+  }
+  onPrevClick() {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+  onNextClick() {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
   }
 }
